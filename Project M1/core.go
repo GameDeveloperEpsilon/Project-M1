78f1965eefd3fe91a70730fecbec038062482719
REC <ryanchapman2084@gmail.com>
2021-12-06 23:21:33 -0600

    Added addition

--- Project M1/core.go
+++ Project M1/core.go
@@ -5,4 +5,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+	
+	var var0 float32
+	var var1 float32
+
+	var0 = 5.5
+	var1 = 72.1
+	fmt.Println(var0 + var1)
+	
+	fmt.Println("Goodbye World!")
 }
