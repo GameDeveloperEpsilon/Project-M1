46c1698c92e349d2534152b6998d925a57cada6f
REC <ryanchapman2084@gmail.com>
2021-12-07 01:25:03 -0600

    Trying to fix this mess

--- Project M1/core.go
+++ Project M1/core.go
@@ -11,9 +11,19 @@ func main() {
 
 	var0 = 5.5
 	var1 = 72.1
-	fmt.Println(var0 + var1)
 	
-	fmt.Println("Do robots dream of electric sheep?")
+	fmt.Printf("%.1f + %.1f = %.1f\n", var0, var1, var0 + var1)
 
+	writeIntroduction("anonymous")
+
+	fmt.Println(add3Nums(5, 20, 13))
 	fmt.Println("Goodbye World!")
+}
+
+func writeIntroduction(name string) {
+	fmt.Printf("Hello, my name is %s.\n", name)
+}
+
+func add3Nums(num0, num1, num2 int32) int32 {
+	return num0 + num1 + num2
 }
